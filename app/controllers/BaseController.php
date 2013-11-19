f4c16772bbf614f84045592644729210d691f2db
Adam Lavin <adam@lavoaster.co.uk>
2013-11-19 22:22:03 +0000

    Tabs -> Space in BaseController

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -4,18 +4,18 @@ class BaseController extends Controller {
 
     public $layout = 'layouts.master';
 
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-            $this->layout->title = 'LightBlog';
-		}
-	}
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    protected function setupLayout()
+    {
+        if ( ! is_null($this->layout))
+        {
+            $this->layout = View::make($this->layout);
+            $this->layout->title = Config::get('blog.name');
+        }
+    }
 
 }
