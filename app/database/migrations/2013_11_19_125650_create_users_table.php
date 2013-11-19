509e6821f777bbc9f54fd993409656b4eb4ee59f
Adam Lavin <adam@socratessystems.com>
2013-11-19 13:10:32 +0000

    Added user table migration

---
+++ app/database/migrations/2013_11_19_125650_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(\Illuminate\Database\Schema\Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('password');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+
+}
