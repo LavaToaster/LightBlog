4f2b32a86d363e36db11cb8ece0bcceca1b1f5ce
Adam Lavin <adam@lavoaster.co.uk>
2013-11-19 21:44:58 +0000

    Defaulted the title to LightBlog for master layout in BaseController

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -14,6 +14,7 @@ class BaseController extends Controller {
 		if ( ! is_null($this->layout))
 		{
 			$this->layout = View::make($this->layout);
+            $this->layout->title = 'LightBlog';
 		}
 	}
 
