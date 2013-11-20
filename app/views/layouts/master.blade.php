262199c55b588146e922d57d267f9844f81b3b1f
Adam Lavin <adam@lavoaster.co.uk>
2013-11-20 00:22:47 +0000

    Added sir-trevor-js icons

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ $title }}</title>
         <link href="{{ asset('components/bootstrap/dist/css/bootstrap.css') }}" rel="stylesheet">
+        <link href="{{ asset('components/sir-trevor-js/sir-trevor-icons.css') }}" rel="stylesheet">
         <link href="{{ asset('components/sir-trevor-js/sir-trevor.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
