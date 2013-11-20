e132fa7c23b0b348e102100f9de8f91b904783a6
Adam Lavin <adam@lavoaster.co.uk>
2013-11-20 22:41:10 +0000

    Post the post to the post creation route to add a new post to the posts table that will show a new post on the blog that visitors will then read that post

--- public/js/blog.js
+++ public/js/blog.js
@@ -4,8 +4,20 @@ var Blog = {};
     Blog.addPost = function(event) {
         event.preventDefault();
 
+    };
 
-    }
+    Blog.createPost = function(event) {
+        event.preventDefault();
 
-    $('#new-post').submit(Blog.addPost);
+        $.ajax({
+            type: 'post',
+            url: '/post',
+            data: $('#new-post').serialize() + '&title=' + $('#post-title').html(),
+            success: function(data) {
+                // TODO: Something
+            }
+        });
+    };
+
+    $('#new-post').submit(Blog.createPost);
 })();
