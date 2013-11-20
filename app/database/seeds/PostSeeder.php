5f009a84ad23ecd779c90a2800337c63e2fb3223
Adam Lavin <adam@lavoaster.co.uk>
2013-11-20 14:02:09 +0000

    Added Lorum Ipsum post to PostSeeder

--- app/database/seeds/PostSeeder.php
+++ app/database/seeds/PostSeeder.php
@@ -8,6 +8,13 @@ class PostSeeder extends DatabaseSeeder
         $post->setAuthor(\Lavoaster\LightBlog\User\Storage\Eloquent\User::find(1));
         $post->setTitle('Test Post');
         $post->setContent('Test test test test test test test test test test test test test test test test test');
+        $post->setPublishDate(\Carbon\Carbon::yesterday());
+        $post->save();
+
+        $post = new \Lavoaster\LightBlog\Blog\Storage\Eloquent\Post;
+        $post->setAuthor(\Lavoaster\LightBlog\User\Storage\Eloquent\User::find(1));
+        $post->setTitle('Lorem Ipsum');
+        $post->setContent('Etiam aliquam sem ac velit feugiat elementum. Nunc eu elit velit, nec vestibulum nibh. Curabitur ultrices, diam non ullamcorper blandit, nunc lacus ornare nisi, egestas rutrum magna est id nunc. Pellentesque imperdiet malesuada quam, et rhoncus eros auctor eu. Nullam vehicula metus ac lacus rutrum nec fermentum urna congue. Vestibulum et risus at mi ultricies sagittis quis nec ligula. Suspendisse dignissim dignissim luctus. Duis ac dictum nibh. Etiam id massa magna. Morbi molestie posuere posuere.');
         $post->setPublishDate(\Carbon\Carbon::now());
         $post->save();
     }
