38f775e934187cd039d3584d061750db70af2954
Adam Lavin <adam@lavoaster.co.uk>
2013-11-20 00:59:31 +0000

    Put that editor in a div, editors love divs.
    2011-12 Called, they want their meme back.

--- app/views/blogs/index.blade.php
+++ app/views/blogs/index.blade.php
@@ -7,7 +7,10 @@
             <div class="col-lg-12 text-center">
                 <form>
                     <h2><span id="post-title" contenteditable="true">Test</span><br><small id="post-date">{{ \Carbon\Carbon::now() }}</small></h2>
-                    <textarea name="post-content" class="js-st-instance"></textarea>
+
+                    <div class="editor">
+                        <textarea name="post-content" class="js-st-instance"></textarea>
+                    </div>
 
                     <button type="submit" class="btn btn-default btn-lg">Add Post</button>
                     <hr>
