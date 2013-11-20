66b93483e02eb3dd5ef7432cf77ec2bbb77c83ee
Adam Lavin <adam@lavoaster.co.uk>
2013-11-20 00:14:01 +0000

    Added user javascript file with login function

---
+++ public/js/user.js
@@ -0,0 +1,40 @@
+var User = {};
+
+(function() {
+    User.login = function(event) {
+        event.preventDefault();
+        var $errorBox = $('#login-errors');
+        $errorBox.html(''); // Remove all errors
+
+        $.ajax({
+            type: 'post',
+            url: '/login',
+            data: $('#login-form').serialize(),
+            success: function(data) {
+                if ( data.success ) {
+                    return User.initialise(data.user);
+                }
+
+                var $errors = data.errors;
+
+                if( $errors.email ) {
+                    $errorBox.append('<span class="alert alert-danger login-alert">' + $errors.email[0] + '</span>');
+                }
+
+                if( $errors.password ) {
+                    $errorBox.append('<span class="alert alert-danger login-alert">' + $errors.password[0] + '</span>');
+                }
+
+                if( $errors.wrong ) {
+                    $errorBox.append('<span class="alert alert-danger login-alert">' + $errors.wrong[0] + '</span>');
+                }
+            }
+        });
+    };
+
+    User.initialise = function(user) {
+        // TODO: Init frontend for user display
+    };
+
+    $('#login-form').submit(User.login);
+})();
