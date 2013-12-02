4bc73ef3fea610c48cec18bdedf068558a1038e0
Adam Lavin <adam@lavoaster.co.uk>
2013-12-02 07:00:42 +0000

    Added it to create post box and changed Test to Title

--- app/views/blogs/create.blade.php
+++ app/views/blogs/create.blade.php
@@ -1,7 +1,7 @@
-<div class="box">
+<div class="box" id="create-post">
     <div class="col-lg-12 text-center">
         <form id="post-form" method="post">
-            <h2><span id="post-title" contenteditable="true">Test</span><br><small id="post-date">{{ \Carbon\Carbon::now() }}</small></h2>
+            <h2><span id="post-title" contenteditable="true">Title</span><br><small id="post-date">{{ \Carbon\Carbon::now() }}</small></h2>
 
             <div class="editor">
                 <textarea name="post-content" class="js-st-instance"></textarea>
