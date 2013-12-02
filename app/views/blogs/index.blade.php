3d1bca5ccc18ab941ce2bf610f8d6185a969c8c7
Adam Lavin <adam@lavoaster.co.uk>
2013-12-02 07:01:22 +0000

    Moved add post button to a drawer

--- app/views/blogs/index.blade.php
+++ app/views/blogs/index.blade.php
@@ -1,10 +1,18 @@
 @section('content')
 <div class="container">
-    <div class="row" id="posts">
-        <div class="col-lg-12 text-center">
-            <button id="new-post" class="btn btn-default btn-lg">New Post</button>
+    <div class="row">
+        <div class="box control-bar" id="control-bar">
+            <div class="col-lg-12 text-center">
+                <button id="new-post" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-pencil"></span> New Post</button>
+            </div>
         </div>
-        <br>
+        <div class="box drawer" id="drawer">
+            <div class="col-lg-12 text-center" id="">
+                <p><span class="glyphicon glyphicon-chevron-down"></span></p>
+            </div>
+        </div>
+    </div>
+    <div class="row" id="posts">
         @foreach($posts as $post)
         <div class="box">
             <div class="col-lg-12 text-center">
