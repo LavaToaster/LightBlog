7763ec2ec957550b1acce07689a80bc5d7cf9d57
Adam Lavin <adam@lavoaster.co.uk>
2013-12-06 00:01:11 +0000

    Added logged in checks to remove icons / drawer when not logged in. Also snuck in the removal of the margin-bottom override on the nav bar and offset the drawer elements by that.

--- app/views/blogs/index.blade.php
+++ app/views/blogs/index.blade.php
@@ -1,6 +1,7 @@
 @section('content')
 <div class="container">
-    <div class="row">
+    @if(\Auth::check())
+    <div class="drawer-container">
         <div class="box control-bar" id="control-bar">
             <div class="col-lg-12 text-center">
                 <button id="new-post" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-pencil"></span> New Post</button>
@@ -12,13 +13,16 @@
             </div>
         </div>
     </div>
+    @endif
     <div class="row" id="posts">
         @foreach($posts as $post)
         <div class="box post" data-post-id="{{ $post->getId() }}">
+            @if(Auth::check())
             <div class="post-interaction">
                 <button class="btn btn-link glyphicon glyphicon-pencil edit-post"></button>
                 <button class="btn btn-link glyphicon glyphicon-trash delete-post"></button>
             </div>
+            @endif
             <div class="col-lg-12 text-center">
                 <h2>{{ $post->getTitle() }}<br><small>{{ $post->getPublishDate() }}</small></h2>
                 <p>{{ Str::words($post->getContent(), 100) }}</p>
--- app/views/blogs/show.blade.php
+++ app/views/blogs/show.blade.php
@@ -1,23 +1,13 @@
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="box control-bar" id="control-bar">
-            <div class="col-lg-12 text-center">
-                <button id="new-post" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-pencil"></span> New Post</button>
-            </div>
-        </div>
-        <div class="box drawer" id="drawer">
-            <div class="col-lg-12 text-center" id="">
-                <p><span class="glyphicon glyphicon-chevron-down"></span></p>
-            </div>
-        </div>
-    </div>
     <div class="row" id="posts">
         <div class="box post" data-post-id="{{ $post->getId() }}">
+            @if(\Auth::check())
             <div class="post-interaction">
                 <button class="btn btn-link glyphicon glyphicon-pencil edit-post"></button>
                 <button class="btn btn-link glyphicon glyphicon-trash delete-post"></button>
             </div>
+            @endif
             <div class="col-lg-12 text-center">
                 <h2>{{ $post->getTitle() }}<br><small>{{ $post->getPublishDate() }}</small></h2>
                 <p>{{ $post->getContent() }}</p>
